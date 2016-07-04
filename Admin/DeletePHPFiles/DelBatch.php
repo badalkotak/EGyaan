c2f0072485e8b249673d119e86cca72129e6e3a8
badalkotak <badalkotak@gmail.com>
2016-07-04 18:33:16 +0530

    SQL File Added

--- Admin/DeletePHPFiles/DelBatch.php
+++ Admin/DeletePHPFiles/DelBatch.php
@@ -1,10 +1,10 @@
 <?php
 include("../dbcon.php");
-error_reporting(0);
+//error_reporting(0);
 
 $BatchId=$_REQUEST["del"];
 
-$result1=mysqli_query($con,"DELETE FROM `Batch` WHERE `Batchid`='$BatchId'");
+$result1=mysqli_query($con,"DELETE FROM `Batch` WHERE `BatchId`='$BatchId'");
 
 header('Location: ../Batch.php');
 ?>
--- Admin/DeletePHPFiles/DelCourse.php
+++ Admin/DeletePHPFiles/DelCourse.php
@@ -1,50 +1,51 @@
 <?php
-include("dbcon.php");
-error_reporting(0);
-$uid=$_REQUEST['del'];
-$result=mysqli_query($con,"DELETE FROM `courses` WHERE `Course_uid`='$uid'");
-$del_attendence=mysqli_query($con,"DELETE FROM `Attendence` WHERE `course_uid`='$uid'");
-$del_course_reg=mysqli_query($con,"DELETE FROM `course_reg` WHERE `course_uid`='$uid'");
-$del_course_teacher=mysqli_query($con,"DELETE FROM `course_teacher` WHERE `uid`='$uid'");
+include("../dbcon.php");
+//error_reporting(0);
+echo $uid=$_REQUEST['del'];
 
-$filename=mysqli_query($con,"SELECT * FROM doubts WHERE Course_uid='$uid'");
+/*$del_attendence=mysqli_query($con,"DELETE FROM `Attendence` WHERE `course_uid`='$uid'");
+$del_course_reg=mysqli_query($con,"DELETE FROM `course_reg` WHERE `course_uid`='$uid'");
+$del_course_teacher=mysqli_query($con,"DELETE FROM `course_teacher` WHERE `uid`='$uid'");*/
+
+$filename=mysqli_query($con,"SELECT * FROM Doubt WHERE CourseId='$uid'");
 while ($row=mysqli_fetch_array($filename)) {
-	$s_file=$row['student_file'];
-	$t_file=$row['teacher_file'];
+	$s_file=$row['StudentFile'];
+	$t_file=$row['TeacherFile'];
 	if($s_file!="")
 unlink($s_file);
 else if($t_file!="")
 unlink($t_file);
 }
-$del_doubts=mysqli_query($con,"DELETE FROM `doubts` WHERE `Course_uid`='$uid'");
-$filename=mysqli_query($con,"SELECT path FROM notes WHERE course_uid='$uid'");
+//$del_doubts=mysqli_query($con,"DELETE FROM `doubts` WHERE `Course_uid`='$uid'");
+$filename=mysqli_query($con,"SELECT NoteFile FROM Notes WHERE CourseId='$uid'");
 while ($row=mysqli_fetch_array($filename)) {
-	$path=$row['path'];
+	$path="../".$row['NoteFile'];
 }
 unlink($path);
-$del_notes=mysqli_query($con,"SELECT * FROM `notes` WHERE `course_uid`='$uid'");
+//$del_notes=mysqli_query($con,"SELECT * FROM `Submission` WHERE `CourseId`='$uid'");
 
-$filename=mysqli_query($con,"SELECT path FROM submission WHERE Course_uid='$uid");
+$filename=mysqli_query($con,"SELECT SubmissionFile FROM Submission WHERE CourseId='$uid");
 while ($row=mysqli_fetch_array($filename)) {
-	$path=$row['path'];
+	$path="../".$row['SubmissionFile'];
 }
 unlink($path);
-$del_submission=mysqli_query($con,"DELETE FROM `submission` WHERE `Course_uid`='$uid'");
+//$del_submission=mysqli_query($con,"DELETE FROM `Submission` WHERE `CourseId`='$uid'");
 
-$del_sub_report=mysqli_query($con,"DELETE FROM `submission_report` WHERE `course_uid`='$uid'");
-$filename=mysqli_query($con,"SELECT path FROM syllabus WHERE course='$uid'");
+//$del_sub_report=mysqli_query($con,"DELETE FROM `submission_report` WHERE `course_uid`='$uid'");
+$filename=mysqli_query($con,"SELECT SyllabusFile FROM Syllabus WHERE CourseId='$uid'");
 while ($row=mysqli_fetch_array($filename)) {
-	$path=$row['path'];
+	$path="../".$row['SyllabusFile'];
 }
 unlink($path);
-$del_syllabus=mysqli_query($con,"DELETE FROM `syllabus` WHERE `course`='$uid'");
+//$del_syllabus=mysqli_query($con,"DELETE FROM `syllabus` WHERE `course`='$uid'");
 
-$filename=mysqli_query($con,"SELECT path FROM Textbooks WHERE course_uid='$uid");
+$filename=mysqli_query($con,"SELECT TextbookFile FROM Textbook WHERE CourseId='$uid");
 while ($row=mysqli_fetch_array($filename)) {
-	$path=$row['path'];
+	$path="../".$row['TextbookFile'];
 }
 unlink($path);
-$del_textbook=mysqli_query($con,"DELETE FROM `Textbooks` WHERE `course_uid`='$uid'");
+//$del_textbook=mysqli_query($con,"DELETE FROM `Textbooks` WHERE `course_uid`='$uid'");
 
-header('Location: course.php')
+if($result=mysqli_query($con,"DELETE FROM `Course` WHERE `CourseId`='$uid'"))
+header('Location: ../Course.php')
 ?>
